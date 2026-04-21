28c6c26f11dc5d98ef6e4d6a363b1d09d5cc8262
Mellow <95871222+Mell-o@users.noreply.github.com>
2026-04-20 23:48:29 -0400

    Linked js to html

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 import data from "./data.json" with {type: "json"}
+
+const categories = document.querySelector(".categories")
+
+console.log(categories)
+for (const category of data) {
+    const {category: curCategory, score, icon} = category
+    console.log(curCategory, score, icon)
+}
